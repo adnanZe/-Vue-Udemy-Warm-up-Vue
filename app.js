3db48c4b4a2b70bdc2fa5d96f3e7aea0274716aa
AdnanZe <adnan.zecheria@yahoo.com>
2023-03-20 07:39:39 +0200

    switching from vanilla js to vue

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-const buttonElement = document.querySelector("button");
-const inputElement = document.querySelector("input");
-const listElement = document.querySelector("ul");
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: "",
+    };
+  },
 
-function addGoal() {
-  const enteredValue = inputElement.value;
-  const listItemElement = document.createElement("li");
-  listItemElement.textContent = enteredValue;
-  listElement.appendChild(listItemElement);
-  inputElement.value = "";
-}
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = "";
+    },
+  },
+}).mount("#app");
 
-buttonElement.addEventListener("click", addGoal);
+// const buttonElement = document.querySelector("button");
+// const inputElement = document.querySelector("input");
+// const listElement = document.querySelector("ul");
+
+// function addGoal() {
+//   const enteredValue = inputElement.value;
+//   const listItemElement = document.createElement("li");
+//   listItemElement.textContent = enteredValue;
+//   listElement.appendChild(listItemElement);
+//   inputElement.value = "";
+// }
+
+// buttonElement.addEventListener("click", addGoal);
